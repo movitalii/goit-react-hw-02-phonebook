11936d5a395aaf9337e4f8532bd6a5e4b05d290b
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-10-30 20:49:39 +0200

    render form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import css from './App.module.css';
+import React, { Component } from 'react';
+import Phonebook from './Phonebook/Phonebook';
+
+class  App extends Component {
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+    <div className={css.wrapper}>
+      <Phonebook onSubmit={this.formSubmitHandler} />
+    </div>    
   );
+  }  
 };
+
+export default App;
---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import css from './Phonebook.module.css';
+
+class Phonebook extends Component {
+    state = {
+        name: '',
+        number: '',
+    };
+
+    handleChange = event => {
+        const { name, value } = event.currentTarget
+        this.setState({[name]: value})
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state);
+
+        console.log(this.state);
+
+        this.reset();
+    }
+
+    reset = () => {
+        this.setState({name: '', number: ''})
+    }
+
+    render() {
+        return(
+        <form onSubmit={this.handleSubmit}>
+            <label htmlFor="">
+                Name <input
+                        type="text"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        name="name"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                    />
+            </label>
+            <label htmlFor="">
+                Number <input
+                        type="tel"
+                        value={this.state.number}
+                        onChange={this.handleChange}
+                        name="number"
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
+                    />      
+            </label>
+                
+            <button type="submit">Add contact</button>
+                
+        </form >
+        
+    )
+    }
+    
+};
+
+export default Phonebook;
