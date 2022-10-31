7f2119497d963b4ab2f91ebb0d2656b8aeeb8904
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-10-31 22:36:14 +0200

    add phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,57 @@
+import React from 'react';
+import { Form } from './Form/Form';
+import { Contacts } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
 import css from './App.module.css';
-import React, { Component } from 'react';
-import Phonebook from './Phonebook/Phonebook';
 
-class  App extends Component {
-
-  formSubmitHandler = data => {
-    console.log(data);
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  // ========Adding contacts=========
+  onAddingContacts = newContact => {
+    const contacts = this.state.contacts;
+    contacts.find(contact => contact.name === newContact.name)
+      ? alert(`${newContact.name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }));
   };
 
-  render() {
-    return (
-    <div className={css.wrapper}>
-      <Phonebook onSubmit={this.formSubmitHandler} />
-    </div>    
-  );
-  }  
-};
+  // ===========Filter Contacts==============
 
-export default App;
+  onFilterHandler = e => {
+    const { value } = e.currentTarget;
+    this.setState({ filter: value });
+  };
+  // ===========Delete Contacts==============
+  onDeleteHandler = contactId => {
+    const notAid = contact => contact.id !== contactId;
+
+    const updatedList = this.state.contacts.filter(notAid);
+
+    this.setState({ contacts: updatedList });
+  };
+
+  // =========Render=========
+
+  render() {
+    const { contacts, filter } = this.state;
+    const { onAddingContacts, onFilterHandler, onDeleteHandler } = this;
+
+    return (
+      <div className={css.wrapper}>
+        <h2 className={css.form__title}>Phonebook</h2>
+        <Form onAddingContacts={onAddingContacts} contacts={contacts} />
+        <h2 className={css.form__title}>Contacts</h2>
+        <Filter filteredContent={filter} onFilterHandler={onFilterHandler} />
+        <Contacts
+          contacts={contacts}
+          filteredContent={filter}
+          handleDelete={onDeleteHandler}
+        />
+      </div>
+    );
+  }
+}
